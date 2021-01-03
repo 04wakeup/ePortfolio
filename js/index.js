94b3b7f09a45a6ca4d34ceb1bc590dc431b323b2
04wakeup <42279557+04wakeup@users.noreply.github.com>
2021-01-02 20:58:42 -0800

    add TypedJS

--- js/index.js
+++ js/index.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
     play: 5000,
     pagination: false,
   });
+
+  var typed = new Typed(".typed", {
+    strings: ["Fullstack Developer", "Web Developer", "Software Engineer"],
+    typeSpeed: 90,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 });
