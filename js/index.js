4ab5181be3b33af56732f603c4fadb5f0ee9cde9
04wakeup <42279557+04wakeup@users.noreply.github.com>
2021-01-02 23:50:28 -0800

    Add Activating Charts on scroll

--- js/index.js
+++ js/index.js
@@ -5,44 +5,51 @@ $(document).ready(function () {
     pagination: false,
   });
 
-  var typed = new Typed(".typed", {
+  const typed = new Typed(".typed", {
     strings: ["Fullstack Developer", "Web Developer", "Software Engineer", "Database Administrator"],
     typeSpeed: 90,
     loop: true,
     startDelay: 1000,
     showCursor: false,
   });
-});
 
-// Part 3: Skills Carousel
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  items: 4,
-  responsive: {
-    0: {
-      items: 1,
+  // Part 3: Skills Carousel
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
     },
-    480: {
-      items: 2,
-    },
-    768: {
-      items: 3,
-    },
-    938: {
-      items: 4,
-    },
-  },
-});
+  });
 
-// PieChart
-$(".chart").easyPieChart({
-  easing: "easeInOut",
-  barColor: "#fff",
-  trackColor: false,
-  scaleColor: false,
-  lineWidth: 4,
-  size: 152,
-  onStep: function (from, to, percent) {
-    $(this.el).find(".percent").text(Math.round(percent));
-  },
+  // get the offset from top
+  const skillsTopOffset = $(".skillsSection").offset().top;
+  $(window).scroll(function () {
+    // just before the position of charts
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      // PieChart
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    }
+  });
 });
